2caf1a8e8fc8c3e5feb47cbc6bf8ba4122ad1ea4
SanjarbekAI <sanjarbekwork@gmail.com>
2025-01-23 14:14:59 +0500

    changed;

--- app_pages/views.py
+++ app_pages/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
+from django.utils.translation import gettext_lazy as _
 
 from app_pages.forms import ContactModelForm
 from app_pages.models import ContactModel
@@ -16,11 +19,14 @@ class ContactCreateView(CreateView):
     template_name = 'pages/contact.html'
     form_class = ContactModelForm
     model = ContactModel
-    success_url = '/contact'
+    success_url = reverse_lazy('pages:contact')
 
-    # def form_valid(self, form):
-    #     pass
-    #
-    # def form_invalid(self, form):
-    #     pass
-    #
+    def form_valid(self, form):
+        messages.success(self.request, _("Your contact information is successfully sent"))
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        for error in form.errors:
+            messages.error(self.request, error)
+        return super().form_invalid(form)
+
